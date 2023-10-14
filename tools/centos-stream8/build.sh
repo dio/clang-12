fffc787aacc5fdaa62a811c9a798205be84208c8
Dhi Aurrahman <dio@rockybars.com>
2023-10-14 16:43:33 +0700

    Run build
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- tools/centos-stream8/build.sh
+++ tools/centos-stream8/build.sh
@@ -3,8 +3,8 @@
 set -e
 
 curl -fsLO https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-12.0.1.tar.gz
-tar xzf llvmorg-12.0.1.tar.gz
-cd llvmorg-12.0.1
+tar -xzf llvmorg-12.0.1.tar.gz
+cd llvm-project-llvmorg-12.0.1
 mkdir build install
 cd build
 INSTALL_PREFIX=`pwd`/install
