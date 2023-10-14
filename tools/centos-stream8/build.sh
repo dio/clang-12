e7badda3f56a3cd0d3bbe9f99219c0848f812afd
Dhi Aurrahman <dio@rockybars.com>
2023-10-14 16:57:58 +0700

    Set to 12.0.0
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- tools/centos-stream8/build.sh
+++ tools/centos-stream8/build.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-curl -fsLO https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-12.0.1.tar.gz
-tar -xzf llvmorg-12.0.1.tar.gz
-cd llvm-project-llvmorg-12.0.1
+curl -fsLO https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-12.0.0.tar.gz
+tar -xzf llvmorg-12.0.0.tar.gz
+cd llvm-project-llvmorg-12.0.0
 mkdir build install
 cd build
 INSTALL_PREFIX=`pwd`/install
--- tools/centos-stream8/builder.Dockerfile
+++ tools/centos-stream8/builder.Dockerfile
@@ -1,4 +1,4 @@
 FROM quay.io/centos/centos:stream8
-RUN yum install -y gcc gcc-c++ xz python3 perl ncurses-compat-libs libatomic unzip cmake
+RUN yum install -y gcc gcc-c++ xz python3 perl ncurses-compat-libs libatomic unzip cmake git
 COPY ./tools/centos-stream8/build.sh /var/local/build.sh
 RUN cat /var/local/build.sh | bash
