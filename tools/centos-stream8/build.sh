7270c579be0530832df9e09ea86ff3ea50eabcae
Dhi Aurrahman <dio@rockybars.com>
2023-10-15 07:35:54 +0700

    Fix
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- tools/centos-stream8/build.sh
+++ tools/centos-stream8/build.sh
@@ -2,11 +2,16 @@
 
 set -e
 
-curl -fsLO https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-12.0.0.tar.gz
-tar -xzf llvmorg-12.0.0.tar.gz
+curl -fsL https://github.com/dio/boringssl-fips/releases/download/fips-20210429-ninja-1.10.2/ninja-arm64.tar.xz | tar -xJ -C /usr/local/bin
+curl -fsLO https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-12.0.0.tar.gz | tar -xz
 cd llvm-project-llvmorg-12.0.0
 mkdir build install
-cd build
 INSTALL_PREFIX=`pwd`/install
-cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DLLVM_ENABLE_PROJECTS=clang -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" ../llvm
-make -j4
+cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+  -DLLVM_ENABLE_PROJECTS="clang" \
+  -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind" \
+  -DCMAKE_BUILD_TYPE=Release \
+  -G Ninja \
+  -S llvm \
+  -B build
+ninja -C build install-clang install-clang-resource-headers install-runtimes
